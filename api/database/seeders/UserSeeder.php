cca9968ae24448085af814d8afd357b5a93b5e8e
Eri Suzuki <sekka.alice@gmail.com>
2023-11-08 16:10:34 +0900

    [add] usersテーブルのシーディングを作成。

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 生徒
+        DB::table('users')->insert([
+            'name' => '佐藤翔太',
+            'email' => 'syota.sato@gmail.com',
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // メンター
+        DB::table('users')->insert([
+            'name' => '鈴木太郎',
+            'email' => 'taro.suzuki.mentor@gmail.com',
+            'is_mentor' => true,
+            'password' => Hash::make('password'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
