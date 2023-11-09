cab3087305281e3a60e39f9bdd73b789c28b7153
Eri Suzuki <sekka.alice@gmail.com>
2023-11-10 00:58:19 +0900

    [add] Apiのjsonエラーレスポンスの雛形を作成。
    ・"response()->error($code, $message, $detail);" でApiのエラーレスポンスが記述可能。

---
+++ api/app/Providers/ApiErrorResponseServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\ServiceProvider;
+
+class ApiErrorResponseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // NOTE: response()->error($code, $message, $detail); で使用可能
+        Response::macro('error', function (int $code, string $message, array | string $detail = null) {
+            if (is_null($detail)) {
+                return response()->json([
+                    'error' => [
+                        'code' => $code,
+                        'message' => $message,
+                    ]
+                ], $code);
+            }
+
+            return response()->json([
+                'error' => [
+                    'code' => $code,
+                    'message' => $message,
+                    'detail' => $detail,
+                ]
+            ], $code);
+        });
+    }
+}
