902e70aea3dbbf7270de81888d23c33fb5eb142a
Eri Suzuki <sekka.alice@gmail.com>
2023-11-10 00:29:15 +0900

    [update] Health Checkの簡易的追加。

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/health-check', function (Request $request) {
+    return 'OK';
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
