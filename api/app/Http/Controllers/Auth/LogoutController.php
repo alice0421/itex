3e08e518843f4d151ebd02284039437677db1701
Eri Suzuki <sekka.alice@gmail.com>
2023-12-03 18:13:44 +0900

    [add] Logoutの処理実装。

---
+++ api/app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     * 
+     * @param Request $request
+     * @return 
+     */
+    public function __invoke(Request $request): Response
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->noContent(Response::HTTP_OK);
+    }
+}
--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
